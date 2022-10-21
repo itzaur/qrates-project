e5e4c835b8a3a8f5b96b988081389f8c3911b8d5
itzaur <preneur85@gmail.com>
2022-10-21 14:49:49 +0300

    test scrollTrigger

--- src/index.js
+++ src/index.js
@@ -21,6 +21,7 @@ preloadImages().then(() => {
   document.body.classList.remove("loading");
 });
 
+//ANCHOR Header buttons animation
 const btnsBox = document.querySelector(".header__login");
 const btns = document.querySelectorAll(".btn");
 const cursor = document.querySelector(".cursor");
@@ -66,28 +67,18 @@ document.querySelectorAll("a, [data-hover='hover']").forEach((el) => {
   });
 });
 
+//ANCHOR Menu navbar animation
 const menuBtn = document.querySelector(".header__menu-logo");
 const menuBtnPath = document.querySelector(".header__menu-logo svg path");
 const menuList = document.querySelector(".header__nav ul:first-child");
-const menuItem = menuList.querySelectorAll("li");
 const menuTimeline = gsap.timeline({ paused: true });
 
 menuTimeline.to(menuList, {
-  y: 200,
+  y: 100,
   duration: 1.5,
   ease: "back.out",
 });
-// .from(
-//   menuItem,
-//   {
-//     y: -80,
-//     duration: 1.5,
-//     stagger: { each: 0.2 },
-//     ease: "power3.out",
-//     clearProps: "all",
-//   },
-//   "<0"
-// );
+
 const menuLists = document.querySelectorAll(".openlist");
 const linkAnim = gsap.timeline({ paused: true });
 
@@ -128,7 +119,7 @@ menuLists.forEach((list) =>
   })
 );
 
-//ANCHOR Menu
+//ANCHOR Menu onclick animation
 const menuSvg = document.querySelector("#menu-wrapper");
 const menuSvgPath = document.querySelector("#path");
 const size = {
@@ -180,7 +171,6 @@ menuBtn.addEventListener("click", () => {
     delay: 0.12,
     duration: 1,
   });
-  // munuActiveTimeline.play();
 
   menuBtn.classList.toggle("open");
   document.querySelector(".header__nav").classList.toggle("open");
@@ -203,6 +193,12 @@ const handleResize = () => {
   middleTop.x = size.w / 2;
   rightTop.x = size.w;
 
+  if (active) {
+    middleTop.y = size.h;
+    rightTop.y = size.h;
+    leftTop.y = size.h;
+  }
+
   if (window.innerWidth > 900 && active) {
     active = false;
 
@@ -224,15 +220,15 @@ const handleResize = () => {
     document.querySelector(".header__nav").classList.remove("open");
   }
 
-  if (window.innerWidth < 900 && active) {
+  if (window.innerWidth < 900 && !active) {
     menuBtnPath.setAttribute("d", "M4 6h16v2H4zm0 5h16v2H4zm0 5h16v2H4z");
     menuBtn.classList.remove("open");
   }
 };
-handleResize();
+
 window.addEventListener("resize", handleResize);
 
-//ANCHOR Testing checkbox open / close
+//ANCHOR Test checkbox open / close
 document.addEventListener("click", (e) => {
   const checkbox = document.querySelectorAll(".checkbox");
   checkbox.forEach((el) => {
@@ -328,81 +324,7 @@ function createAnimation(element) {
   };
 }
 
-//ANCHOR Ticker functionality
-// const ticker = document.querySelector(".ticker");
-// const tickerList = document.querySelector(".ticker__list");
-// const ticker = gsap.timeline();
-// ticker.to(".ticker__list", {
-//   xPercent: -100,
-//   repeat: -1,
-//   ease: "none",
-//   duration: 15,
-// });
-
-// const ticker = document.querySelector(".ticker");
-// const tickerList = document.querySelector(".ticker__list");
-// let clone = tickerList.cloneNode(true);
-// let clone2 = tickerList.cloneNode(true);
-// ticker.append(clone);
-// ticker.append(clone2);
-
-// const groups = gsap.utils.toArray(".accordion-group");
-// const menus = gsap.utils.toArray(".accordion-menu");
-// const menuToggles = groups.map(createAnimation);
-
-// menus.forEach((menu) => menu.addEventListener("click", () => toggleMenu(menu)));
-// function toggleMenu(clickedMenu) {
-//   menuToggles.forEach((toggleFn) => toggleFn(clickedMenu));
-// }
-
-// function createAnimation(element) {
-//   let menu = element.querySelector(".accordion-menu");
-//   let box = element.querySelector(".accordion-content");
-//   let plusElement = element.querySelector(".accordion-plus");
-//   let minusElement = element.querySelector(".accordion-minus");
-
-//   gsap.set(box, {
-//     height: "auto",
-//   });
-
-//   const animation = gsap.timeline();
-//   animation
-//     .from(box, {
-//       height: 0,
-//       duration: 0.5,
-//       ease: "power1.inOut",
-//     })
-//     .to(
-//       plusElement,
-//       {
-//         autoAlpha: 0,
-//         duration: 0.2,
-//         ease: "none",
-//       },
-//       0
-//     )
-//     .from(
-//       minusElement,
-//       {
-//         autoAlpha: 0,
-//         duration: 0.2,
-//         ease: "none",
-//       },
-//       0
-//     )
-//     .reverse();
-
-//   return function (clickedMenu) {
-//     if (clickedMenu === menu) {
-//       // animation.reversed(!animation.reversed());
-//       animation.play();
-//     } else {
-//       animation.reverse();
-//     }
-//   };
-// }
 //ANCHOR Slider functionality
-const slider = document.querySelector(".slider");
 const sliderBox = document.querySelectorAll(".slider__box");
 const sliderCheckbox = document.querySelector(".slider__checkbox");
 const sliderCheckboxElement = document.querySelectorAll(
@@ -426,7 +348,6 @@ sliderCheckbox.addEventListener("click", (e) => {
 });
 
 //ANCHOR Studio images functionality
-// const studioImgs = gsap.utils.toArray(".studio-img");
 const imagesAnim = gsap.timeline();
 imagesAnim.from(".studio-img", {
   autoAlpha: 0,
@@ -435,4 +356,221 @@ imagesAnim.from(".studio-img", {
   ease: "none",
   repeat: -1,
   yoyo: true,
+});
+
+//ANCHOR Test ScrollTrigger
+gsap.registerPlugin(ScrollTrigger);
+
+let headerTimeline = gsap.timeline({
+  scrollTrigger: {
+    trigger: ".header",
+    pin: true,
+    pinSpacing: false,
+    start: "top top",
+    endTrigger: ".footer",
+    scrub: 1,
+  },
+  backgroundColor: "red",
+});
+
+headerTimeline.to(".header", {
+  backgroundColor: "white",
+  duration: 1,
+});
+
+gsap.from(".choose .section__box:nth-child(2) > *", {
+  x: 900,
+  ease: "power1.out",
+  stagger: { each: 0.2 },
+  scrollTrigger: {
+    trigger: ".about",
+    toggleActions: "restart none none none",
+    start: "bottom 80%",
+  },
+  duration: 0.6,
+});
+
+gsap.from(".choose .section__box:nth-child(1) > *", {
+  x: -900,
+  ease: "power1.out",
+  stagger: { each: 0.2 },
+  scrollTrigger: {
+    trigger: ".about",
+    toggleActions: "restart none none none",
+    start: "bottom 80%",
+  },
+  duration: 0.6,
+});
+
+gsap.from(".production .section__box:nth-child(2) > *", {
+  x: 900,
+  ease: "power1.out",
+  stagger: { each: 0.2 },
+  scrollTrigger: {
+    trigger: ".choose",
+    toggleActions: "restart none none none",
+    start: "bottom 80%",
+  },
+  duration: 0.6,
+});
+
+gsap.from(".production .section__box:nth-child(1) > *", {
+  x: -900,
+  ease: "power1.out",
+  stagger: { each: 0.2 },
+  scrollTrigger: {
+    trigger: ".choose",
+    toggleActions: "restart none none none",
+    start: "bottom 80%",
+  },
+  duration: 0.6,
+});
+
+gsap.from(".services .services__info > *", {
+  x: -900,
+  rotation: -15,
+  ease: "power1.out",
+  stagger: { each: 0.2 },
+  scrollTrigger: {
+    trigger: ".production",
+    toggleActions: "restart none none none",
+    start: "bottom 90%",
+  },
+  duration: 0.6,
+});
+
+gsap.from(".services .services__accordion > *, .services__accordion", {
+  y: 40,
+  autoAlpha: 0,
+  ease: "power1.out",
+  stagger: { each: 0.1 },
+  scrollTrigger: {
+    trigger: ".production",
+    toggleActions: "restart none none none",
+    start: "bottom 90%",
+  },
+  duration: 0.6,
+});
+
+gsap.from(".support .support__network > *", {
+  x: -900,
+  ease: "power1.out",
+  stagger: { each: 0.2 },
+  scrollTrigger: {
+    trigger: ".services",
+    toggleActions: "restart none none none",
+    start: "bottom 90%",
+  },
+  duration: 0.6,
+});
+
+gsap.from(".support .support__help > *", {
+  x: 900,
+  ease: "power1.out",
+  stagger: { each: 0.2 },
+  scrollTrigger: {
+    trigger: ".services",
+    toggleActions: "restart none none none",
+    start: "bottom 90%",
+  },
+  duration: 0.6,
+});
+
+gsap.from(".community > *", {
+  y: 200,
+  autoAlpha: 0,
+  ease: "power1.out",
+  stagger: { each: 0.2 },
+  scrollTrigger: {
+    trigger: ".support",
+    toggleActions: "restart none none none",
+    start: "bottom 90%",
+  },
+  duration: 0.6,
+});
+
+gsap.from(".gallery__item", {
+  y: 200,
+  autoAlpha: 0,
+  scale: 0,
+  rotation: -90,
+  ease: "power1.out",
+  stagger: { each: 0.2 },
+  scrollTrigger: {
+    trigger: ".community",
+    toggleActions: "restart none none none",
+    start: "bottom 100%",
+  },
+  duration: 0.8,
+  onStart: () => {
+    document.querySelector(".gallery").style.backgroundColor = "transparent";
+  },
+  onComplete: () => {
+    document.querySelector(".gallery").style.backgroundColor = "black";
+  },
+});
+
+gsap.from(".slider", {
+  y: 900,
+  autoAlpha: 0,
+  ease: "power1.out",
+
+  scrollTrigger: {
+    trigger: ".gallery",
+    toggleActions: "restart none none none",
+    start: "bottom 90%",
+  },
+  duration: 1.2,
+});
+
+function randomNumber(max, min) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+gsap.from(".studio > *", {
+  y: `+=${randomNumber(-300, 300)}`,
+  x: `+=${randomNumber(-300, 300)}`,
+
+  autoAlpha: 0,
+  ease: "power1.out",
+  stagger: { each: 0.2 },
+  scrollTrigger: {
+    trigger: ".studio",
+    toggleActions: "restart none none none",
+    start: "top 97%",
+  },
+  duration: 1.2,
+});
+
+const footerLinks = gsap.utils.toArray(".footer-list > *");
+
+footerLinks.forEach((link) => {
+  gsap.from(link, {
+    y: `+=${randomNumber(-400, 400)}`,
+    x: `+=${randomNumber(-400, 400)}`,
+
+    autoAlpha: 0,
+    ease: "power1.out",
+    stagger: { each: 0.02 },
+    scrollTrigger: {
+      trigger: ".studio",
+      toggleActions: "restart none none none",
+      start: "bottom 95%",
+    },
+    duration: 1,
+  });
+});
+
+gsap.from(".footer-box-icons", {
+  yPercent: 50,
+  autoAlpha: 0,
+  ease: "power1.out",
+  clearProps: "all",
+
+  scrollTrigger: {
+    trigger: ".footer",
+    toggleActions: "restart none none none",
+    start: "60% 95%",
+  },
+  duration: 1,
 });
